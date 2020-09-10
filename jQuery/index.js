0d0a2d31dcb68ab32480f59655b94e18e1c5609b
Greg Nelson <nelsongreg35@gmail.com>
2020-09-09 20:45:03 -0500

    More jQuery

--- jQuery/index.js
+++ jQuery/index.js
@@ -11,4 +11,16 @@ $("a").attr("href", "https://www.yahoo.com");
 
 $("button").click(function() {
   $("h1").css("color", "purple");
-})
+});
+
+$("input").keypress(function(event) {
+  console.log(event.key);
+});
+
+$(document).keypress(function(event) {
+  $("h1").text(event.key);
+});
+
+$("h1").on("mouseover",function() {
+  $("h1").css("color", "green");
+});
