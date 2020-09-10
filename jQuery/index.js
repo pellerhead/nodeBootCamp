1f99b15f7ebf183c426c9ef0b74534a93a5e83bf
Greg Nelson <nelsongreg35@gmail.com>
2020-09-09 20:36:21 -0500

    jQuery

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,3 +1,14 @@
 $("h1").css("color","white");
 
-$("h1").addClass("big-title");  
+$("h1").addClass("big-title");
+
+$("button").text("Do not click me!");
+
+$("img").attr("src","images/crash.png");
+$("img").attr("alt","symbols");
+
+$("a").attr("href", "https://www.yahoo.com");
+
+$("button").click(function() {
+  $("h1").css("color", "purple");
+})
