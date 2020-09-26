4152158d23f2b0d81f6b7d11393b0d83f9ec0e5d
Greg Nelson <nelsongreg35@gmail.com>
2020-09-25 22:08:54 -0500

    Getting ready to deploy on Heroku

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -36,6 +36,6 @@ app.post("/failure", function(req, res) {
   res.redirect("/");
 })
 
-app.listen(3000, function() {
-  console.log("Server is running on port 3000");
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server is running...");
 })
