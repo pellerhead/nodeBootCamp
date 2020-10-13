c762a5a2cdc90f2976590392485a2f39a9a59f41
Greg Nelson <nelsongreg35@gmail.com>
2020-10-12 21:07:03 -0500

    Create connection to MongoDB, add some records and then find and display the records

--- FruitsProject/app.js
+++ FruitsProject/app.js
@@ -13,5 +13,35 @@ mongoClient.connect(function(err, mongoClient) {
 
   console.log("Connected successfully to server");
 
-  mongoClient.close();
+//  insertDocuments(db,function() {
+//    mongoClient.close();
+//  });
+
+  findDocuments(db,function() {
+    mongoClient.close();
+  });
+
 });
+
+const insertDocuments = function(db, callback) {
+    const collection = db.collection('fruits');
+
+    collection.insertMany( [ {a : 1}, {a : 2}, {a : 3} ], function(err, result) {
+      assert.equal(err, null);
+      assert.equal(3, result.result.n);
+      assert.equal(3, result.ops.length);
+      console.log("Inserted 3 documents into the collection");
+      callback(result);
+    });
+};
+
+const findDocuments = function(db, callback) {
+    const collection = db.collection('fruits');
+
+    collection.find({}).toArray(function(err, result) {
+      assert.equal(err, null);
+      console.log("FOund the following records");
+      console.log(result);
+      callback(result);
+    });
+};
