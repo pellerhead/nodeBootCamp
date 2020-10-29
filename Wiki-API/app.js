6e773afa00e98cda85f154468ab99e5544a25d2f
Greg Nelson <nelsongreg35@gmail.com>
2020-10-29 17:04:06 -0500

    Specifying GET, POST an DELETE

--- Wiki-API/app.js
+++ Wiki-API/app.js
@@ -39,6 +39,35 @@ app.get("/articles", function(req, res) {
   });
 });
 
+app.post("/articles", function(req, res) {
+  console.log(req.body.title);
+  console.log(req.body.content);
+
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content
+  });
+
+  newArticle.save(function(err){
+    if (!err) {
+      res.send("Successfully added a new article");
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+app.delete("/articles", function(req, res){
+  Article.deleteMany(function(err){
+    if (!err) {
+      res.send("Succesfully deleted all articles");
+    } else {
+      res.send(err);
+    }
+  });
+
+});
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
