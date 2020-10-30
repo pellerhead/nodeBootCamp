86abfc21ade0449cd90b633c93eb536d016fdb50
Greg Nelson <nelsongreg35@gmail.com>
2020-10-29 20:03:06 -0500

    Javascript RESTful Api completed

--- Wiki-API/app.js
+++ Wiki-API/app.js
@@ -28,7 +28,6 @@ const articleSchema = {
 const Article = mongoose.model("Article", articleSchema);
 
 //TODO
-
 app.route("/articles")
 .get(function(req, res) {
   Article.find(function(err, foundArticles) {
@@ -61,6 +60,55 @@ app.route("/articles")
       res.send(err);
     }
   });
+});
+
+
+app.route("/articles/:articleTitle")
+.get(function(req, res){
+  Article.findOne({title: req.params.articleTitle}, function(err, foundArticle){
+    if (foundArticle) {
+      res.send(foundArticle);
+    } else {
+      res.send("No articles matching that title was found");
+    }
+  });
+})
+.put(function(req, res){
+  Article.update(
+    {title: req.params.articleTitle},
+    {title: req.body.title, content: req.body.content},
+    {overwrite: true},
+    function(err){
+      if(!err){
+        res.send("Succesfully updated article.");
+      }
+    }
+  );
+})
+.patch(function(req, res){
+  Article.update(
+    {title: req.params.articleTitle},
+    {$set: req.body},
+    function(err){
+      if(!err){
+        res.send("Succesfully updated article.");
+      } else {
+        res.send(err);
+      }
+    }
+  );
+})
+.delete(function(req, res){
+  Article.deleteOne(
+    {title: req.params.articleTitle},
+    function(err){
+      if (!err) {
+        res.send("Successfully deleted the corresponding article.");
+      } else {
+        res.send(err);
+      }
+    }
+  );
 });
 
 app.listen(3000, function() {
