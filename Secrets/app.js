b1f29206d13ba1c40b3745d0109732427fdd3374
Greg Nelson <nelsongreg35@gmail.com>
2020-11-02 11:53:27 -0600

    Level 2 complete

--- Secrets/app.js
+++ Secrets/app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -19,13 +20,15 @@ mongoose.connect("mongodb://localhost:27017/userDB", {
   useUnifiedTopology: true
 });
 
-const userSchema = {
+const userSchema = new mongoose.Schema ({
   email: String,
   password: String
-};
+});
+
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password'] });
 
 const User = new mongoose.model("User", userSchema);
-
 
 app.get("/", function(req, res){
   res.render("home");
@@ -66,7 +69,6 @@ app.post("/login", function(req, res){
         if (foundUser.password === password) {
           res.render("secrets");
         }
-      } else {
       }
     }
   })
